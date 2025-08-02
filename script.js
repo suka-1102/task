0c6bbd5e64d8f9730c380b6ca0933c97dbfae50e
suka <xuheyangsheng@gmail.com>
2025-08-02 12:12:00 +0900

    タスクデータの保存完了

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const newTaskButton = document.getElementById('newTaskButton')
 const modal = document.getElementById('modal')
 const mask = document.getElementById('mask')
 const modalCancel = document.getElementById('modalCancel')
+const newTaskNameInput = document.getElementById('newTaskNameInput')
+const newTaskContentInput = document.getElementById('newTaskContentInput')
+const newTaskTermInput = document.getElementById('newTaskTermInput')
+const inputComplete = document.getElementById('inputComplete')
 
 modal.classList.add('deactive')
 mask.classList.add('deactive')
@@ -13,4 +17,26 @@ newTaskButton.addEventListener('click', () =>{
 modalCancel.addEventListener('click', () => {
   mask.classList.add('deactive');
   modal.classList.add('deactive')
+})
+
+const taskData = []
+let taskNumber = 1;
+inputComplete.addEventListener('click', () => {
+  let newTasknameInpitValue = newTaskNameInput.value
+  let newTaskContentInputValue = newTaskContentInput.value
+  let newTaskTermInputValue = newTaskTermInput.value
+  
+  taskData.push({ name:newTasknameInpitValue,
+                  content: newTaskContentInputValue,
+                  term: newTaskTermInputValue 
+                })
+  localStorage.setItem(`taskData${taskNumber}`,  JSON.stringify(taskData))
+  taskNumber++;
+  newTaskNameInput.value = null;
+  newTaskContentInput.value = null;
+  newTaskTermInput.value = null;
+  taskData.length = 0;
+
+  mask.classList.add('deactive');
+  modal.classList.add('deactive');
 })

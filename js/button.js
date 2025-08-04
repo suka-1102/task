0dba17332cf071de3c3941e0f19aca5f536fbd0b
suka <xuheyangsheng@gmail.com>
2025-08-04 18:59:01 +0900

    ファイルの分割まで

---
+++ js/button.js
@@ -0,0 +1,106 @@
+import {taskAdd} from './functions.js'
+
+let taskListLi = document.querySelectorAll('.taskList li');
+let index = Array.from(taskListLi).findIndex(li => li.id === liId)
+let isDelete = false
+let taskElement
+let newTasknameInputValue;
+let newTaskContentInputValue;
+let newTaskTermInputValue;
+let newTaskTermInputValueEnd;
+const deleteTask = document.getElementById('deleteTask')
+const editTask = document.getElementById('editTask')
+const newTaskName = document.getElementById('newTaskName')
+const newTaskContent = document.getElementById('newTaskContent')
+const newTaskTerm = document.getElementById('newTaskTerm')
+const newTaskNameInput = document.getElementById('newTaskNameInput')
+const newTaskContentInput = document.getElementById('newTaskContentInput')
+const newTaskTermInput = document.getElementById('newTaskTermInput')
+const newTaskTermInputEnd = document.getElementById('newTaskTermInputEnd')
+const inputComplete = document.getElementById('inputComplete')
+const newTaskButton = document.getElementById('newTaskButton')
+//　タスクをクリックした時の関数
+export function taskClick() {
+  taskListLi = document.querySelectorAll('.taskList li');
+  document.querySelectorAll('.taskList > li > button').forEach(button => {
+    button.addEventListener('click', function ()  {
+      mask.classList.remove('deactive');
+      modal.classList.remove('deactive');
+      deleteTask.classList.remove('deactive');
+      editTask.classList.remove('deactive');
+      newTaskName.classList.add('deactive');
+      newTaskContent.classList.add('deactive');
+      newTaskTerm.classList.add('deactive');
+      inputComplete.classList.add('deactive');
+      const liId = this.parentElement.id;
+      index = Array.from(taskListLi).findIndex(li => li.id === liId)
+      taskElement = document.getElementById(liId);
+      deleteButton()
+      
+
+    });
+  })
+}
+
+
+
+// 削除ボタンを押した時の処理をする関数
+export function deleteButton() {
+  if(isDelete) return;
+  deleteTask.addEventListener('click', () => {
+    let taskData = JSON.parse(localStorage.getItem('taskData'));
+    
+    console.log(index)
+    taskData.splice(index,1)
+    
+    localStorage.setItem('taskData', JSON.stringify(taskData));
+    console.log(taskData)
+    // 消した後の表示変更
+    taskList.removeChild(taskElement)
+
+  })
+  isDelete = true;
+}
+
+// 新規タスクボタンを押した時の処理
+export function newTaskButtonProcess() {
+  newTaskButton.addEventListener('click', () =>{
+    newTaskName.classList.remove('deactive')
+    newTaskContent.classList.remove('deactive')
+    newTaskTerm.classList.remove('deactive')
+    inputComplete.classList.remove('deactive')
+    mask.classList.remove('deactive');
+    modal.classList.remove('deactive')
+    deleteTask.classList.add('deactive')
+    editTask.classList.add('deactive')
+  
+  })
+}
+// 完了ボタンを押したとき
+export function completeButton() {
+  inputComplete.addEventListener('click', () => {
+    let taskData = JSON.parse(localStorage.getItem('taskData')) || [];
+    newTasknameInputValue = newTaskNameInput.value
+    newTaskContentInputValue = newTaskContentInput.value
+    newTaskTermInputValue = newTaskTermInput.value
+    newTaskTermInputValueEnd = newTaskTermInputEnd.value
+  
+    taskData.push({ name:newTasknameInputValue,
+                    content: newTaskContentInputValue,
+                    term: newTaskTermInputValue,
+                    termEnd: newTaskTermInputValueEnd
+                  })
+    localStorage.setItem(`taskData`,  JSON.stringify(taskData))
+    taskAdd()
+    
+    newTaskNameInput.value = null;
+    newTaskContentInput.value = null;
+    newTaskTermInput.value = null;
+    newTaskTermInputEnd.value = null;
+    
+    // taskData.length = 0;
+    mask.classList.add('deactive');
+    modal.classList.add('deactive');
+    taskClick()
+  })
+}
--- script.js
+++ script.js
@@ -1,115 +1,32 @@
-const newTaskButton = document.getElementById('newTaskButton')
+import {taskClick,newTaskButtonProcess,completeButton} from '/js/button.js';
+// import {taskAdd} from '/js/functions.js'
+
+
+
 const modal = document.getElementById('modal')
 const mask = document.getElementById('mask')
 const modalCancel = document.getElementById('modalCancel')
-const newTaskNameInput = document.getElementById('newTaskNameInput')
-const newTaskContentInput = document.getElementById('newTaskContentInput')
-const newTaskTermInput = document.getElementById('newTaskTermInput')
-const newTaskTermInputEnd = document.getElementById('newTaskTermInputEnd')
-const inputComplete = document.getElementById('inputComplete')
-const taskList = document.getElementById('taskList')
-const deleteTask = document.getElementById('deleteTask')
-const editTask = document.getElementById('editTask')
-const newTaskName = document.getElementById('newTaskName')
-const newTaskContent = document.getElementById('newTaskContent')
-const newTaskTerm = document.getElementById('newTaskTerm')
 
-let isDelete = false
-let isTaskClick = false;
-let taskListLi = document.querySelectorAll('.taskList li');
-let index = Array.from(taskListLi).findIndex(li => li.id === liId)
-let liId;
+const taskList = document.getElementById('taskList')
+
 
 
 modal.classList.add('deactive')
 mask.classList.add('deactive')
 
+newTaskButtonProcess() 
 
-newTaskButton.addEventListener('click', () =>{
-  newTaskName.classList.remove('deactive')
-  newTaskContent.classList.remove('deactive')
-  newTaskTerm.classList.remove('deactive')
-  inputComplete.classList.remove('deactive')
-  mask.classList.remove('deactive');
-  modal.classList.remove('deactive')
-  deleteTask.classList.add('deactive')
-  editTask.classList.add('deactive')
-
-})
 modalCancel.addEventListener('click', () => {
   mask.classList.add('deactive');
   modal.classList.add('deactive')
 })
 
+completeButton()
 
 
-let newTasknameInputValue;
-let newTaskContentInputValue;
-let newTaskTermInputValue;
-let newTaskTermInputValueEnd;
 let taskSwitch = true
-let taskData
-let taskElement
 
-// 完了ボタンを押したとき
-inputComplete.addEventListener('click', () => {
-  let taskData = JSON.parse(localStorage.getItem('taskData')) || [];
-  newTasknameInputValue = newTaskNameInput.value
-  newTaskContentInputValue = newTaskContentInput.value
-  newTaskTermInputValue = newTaskTermInput.value
-  newTaskTermInputValueEnd = newTaskTermInputEnd.value
 
-  taskData.push({ name:newTasknameInputValue,
-                  content: newTaskContentInputValue,
-                  term: newTaskTermInputValue,
-                  termEnd: newTaskTermInputValueEnd
-                })
-  localStorage.setItem(`taskData`,  JSON.stringify(taskData))
-  taskAdd()
-  
-  newTaskNameInput.value = null;
-  newTaskContentInput.value = null;
-  newTaskTermInput.value = null;
-  newTaskTermInputEnd.value = null;
-  
-  // taskData.length = 0;
-  mask.classList.add('deactive');
-  modal.classList.add('deactive');
-  taskClick()
-})
-
-function taskAdd() {
-  const newTaskId = new Date().getTime();
-  const li = document.createElement('li');
-  li.id = newTaskId;
-  const button = document.createElement('button');
-
-  const spanName = document.createElement('span');
-  spanName.classList = 'taskName'
-  spanName.textContent = newTasknameInputValue
-
-  const spanContent = document.createElement('span');
-  spanContent.classList = 'spanContent'
-  spanContent.textContent = newTaskContentInputValue
-
-  const spanTerm = document.createElement('span');
-  spanTerm.classList = 'spanTerm'
-  spanTerm.textContent = newTaskTermInputValue
-
-  const spanTermEnd = document.createElement('span');
-  spanTermEnd.classList = 'spanTermEnd'
-  spanTermEnd.textContent = newTaskTermInputValueEnd
-
-  
-  
-
-  taskList.appendChild(li)
-  li.appendChild(button)
-  button.appendChild(spanName)
-  button.appendChild(spanContent)
-  button.appendChild(spanTerm)
-  button.appendChild(spanTermEnd)
-}
 const data = JSON.parse(localStorage.getItem('taskData'));
 const taskNumber = JSON.parse(localStorage.getItem('taskData')).length
 if(taskSwitch){
@@ -148,49 +65,4 @@ if(taskSwitch){
     
   });
   taskClick()
-}
-
-
-//　タスクをクリックした時の関数
-function taskClick() {
-  taskListLi = document.querySelectorAll('.taskList li');
-  document.querySelectorAll('.taskList > li > button').forEach(button => {
-    button.addEventListener('click', function ()  {
-      mask.classList.remove('deactive');
-      modal.classList.remove('deactive');
-      deleteTask.classList.remove('deactive');
-      editTask.classList.remove('deactive');
-      newTaskName.classList.add('deactive');
-      newTaskContent.classList.add('deactive');
-      newTaskTerm.classList.add('deactive');
-      inputComplete.classList.add('deactive');
-      const liId = this.parentElement.id;
-      index = Array.from(taskListLi).findIndex(li => li.id === liId)
-      taskElement = document.getElementById(liId);
-      deleteButton()
-      
-
-    });
-  })
-}
-
-
-
-
-// 削除ボタンを押した時の処理をする関数
-function deleteButton() {
-  if(isDelete) return;
-  deleteTask.addEventListener('click', () => {
-    let taskData = JSON.parse(localStorage.getItem('taskData'));
-    
-    console.log(index)
-    taskData.splice(index,1)
-    
-    localStorage.setItem('taskData', JSON.stringify(taskData));
-    console.log(taskData)
-    // 消した後の表示変更
-    taskList.removeChild(taskElement)
-
-  })
-  isDelete = true;
 }
